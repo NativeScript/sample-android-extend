4a6920ca8582ccee48ff4f61556a006cedd82312
atanasovg <georgi.atanassoff@yahoo.com>
2016-07-05 15:01:02 +0300

    Update application.js and Readme.

--- sample-app/app/application.android.js
+++ sample-app/app/application.android.js
@@ -5,11 +5,15 @@ var Application = android.app.Application.extend("org.myApp.Application", {
     onCreate: function() {
         superProto.onCreate.call(this);
 
+        // initialize the application module with the native Android Application object
+        application.android.init(this);
+
         // Enter custom initialization code here
     },
     attachBaseContext: function(base) {
+        superProto.attachBaseContext.call(this, base);
+
         // This code enables MultiDex support for the application
-		superProto.attachBaseContext.call(this, base);
-		android.support.multidex.MultiDex.install(this.super);
+		// android.support.multidex.MultiDex.install(this.super);
 	}
 });
